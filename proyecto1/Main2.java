91f4d8c37bee41363e5a799af4900da02f0abde0
tatacastro12 <91699554+tatacastro12@users.noreply.github.com>
2022-06-30 08:48:37 -0500

    Add files via upload

---
+++ proyecto1/Main2.java
@@ -0,0 +1,11 @@
+public class Main2 {
+    public static void main(String[] args) {
+        int contador = 2;
+
+        while (contador <= 10) {
+            System.out.println(contador);
+            contador = contador + 1;
+        }
+
+    }
+}
---
+++ proyecto1/Main3.java
@@ -0,0 +1,13 @@
+public class Main3 {
+    public static void main(String[] args) {
+        int num = 2;
+        do {
+            num = num + 1;
+            {
+                System.out.println(num);
+            }
+        } while (num < 10);
+
+    }
+
+}
